c8fdc8e331c9054fa39f9f62c0caadc824a8c50a
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-08-14 19:08:32 +0500

    counting max num in str

---
+++ python_basic_projects/count_max_num_in_str.py
@@ -0,0 +1,17 @@
+"""
+ainfall_mi is a string that contains the average number of inches of rainfall in Michigan for every month (in inches) with every month separated by a comma. Write code to compute the number of months that have more than 3 inches of rainfall. Store the result in the variable num_rainy_months. In other words, count the number of items with values > 3.0.
+
+Hard-coded answers will receive no credit.
+"""
+
+rainfall_mi = "1.65, 1.46, 2.05, 3.03, 3.35, 3.46, 2.83, 3.23, 3.5, 2.52, 2.8, 1.85"
+rainfall_list = rainfall_mi.split(", ")
+print(rainfall_list)
+print(type(rainfall_list))
+
+num_rainy_months = 0
+for rainfall in rainfall_list:
+    rainfall_float = float(rainfall)
+    if rainfall_float > 3.0:
+        num_rainy_months += 1
+print(num_rainy_months)

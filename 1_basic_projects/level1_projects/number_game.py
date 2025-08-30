b8cd5ceab164a5e20e58765cbd859d642028708a
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-08-30 07:37:55 +0500

    Number guessing Game

---
+++ 1_basic_projects/level1_projects/number_game.py
@@ -0,0 +1,42 @@
+"""
+Guess the Number Game
+
+Use the random module.
+
+Let the computer choose a number (1–100).
+
+User guesses until they get it right.
+
+Provide hints: "Too high" / "Too low".
+
+"""
+
+import random
+random_num = random.randrange(1,101)
+print("Welcom to the Guess the Number Game")
+print("I'm thinking a number between 1 and 100")
+
+attempts = 0
+guessed_correctly = False
+
+while not guessed_correctly:
+
+    try:
+        user_guess = int(input("Enter a number to guess : "))
+        attempts += 1
+
+        if user_guess < random_num:
+            print(f"{user_guess} is below the number, Try again!")
+        elif user_guess > random_num:
+            print(f"{user_guess} is above the number, Try again!")
+        else:
+            print("Congragulations! You guessed it right!")
+            print(f"{random_num} was indeed the number.")
+            print(f"It looks like you took {attempts} attempts to guess it right.")
+            guessed_correctly = True
+            #print("Thanks for playing!")
+
+    except ValueError:
+        print("Please enter a valid number!")
+    
+print("Thanks for playing!")

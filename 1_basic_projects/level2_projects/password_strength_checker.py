8a56195402336e92093fa0e277aefd62c4760852
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-09-11 08:23:49 +0500

    Python Basics

--- 1_basic_projects/level2_projects/password_strength_checker.py
+++ 1_basic_projects/level2_projects/password_strength_checker.py
@@ -6,16 +6,17 @@ Give feedback.
 """
 user_password = input("Enter your password : ")
 
-while True:
-    if len(user_password) >= 8:
-        print(f'{user_password} has {len(user_password)} characters.')
-    else:
-        print(f"Please enter at least 8 character password.")
-    
-    # numbers
-    numbers = [0,1,2,3,4,5,6,7,8,9]
-    if numbers in user_password:
-        print(f"Password has numbers.")
-    else:
-        print(f"Please include numbers.")
+
+
+if len(user_password) >= 8:
+    print(f'{user_password} has {len(user_password)} characters.')
+else:
+    print(f"Please enter at least 8 character password.")
+
+# numbers
+numbers = [0,1,2,3,4,5,6,7,8,9]
+if numbers in user_password:
+    print(f"Password has numbers.")
+else:
+    print(f"Please include numbers.")
 
